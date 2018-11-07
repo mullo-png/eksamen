5c1c473991f693f347ed03d3c9027e033f3dbff6
alex-prog <coag@kea.dk>
2018-11-07 14:02:06 +0100

    Update util.js

--- web-app/js/util.js
+++ web-app/js/util.js
@@ -1,4 +1,4 @@
 var API_KEY = 'Flight%20booker%201.2%20plus%20edition';
-var URL = 'http://206.189.48.89/API.php?API_KEY='+API_KEY;
+var URL = 'http://46.101.165.227/API.php?API_KEY='+API_KEY;
 
-var URL_STATUS = 'http://206.189.48.89/cgi-bin/status_check?';
+var URL_STATUS = 'http://46.101.165.227/cgi-bin/status_check?';
